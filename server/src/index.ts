cb5d0dc5da987d9c7f406d9875e654fc79ee30e9
Stuart Main <stu.main86@outlook.com>
2026-09-21 15:06:04 +1000

    Replace native bcrypt with bcryptjs to simplify container builds

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import sgMail from "@sendgrid/mail";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { db } from "./firebaseAdmin.js";
 import jwt from "jsonwebtoken";
 import { requireAuth, optionalAuth } from "./middleware/auth.js";
